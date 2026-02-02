0061d42302a36cb5e8d680cdefc84e7b13f3fb83
durbano <jdurbano@digital-fruit.com>
2026-02-02 18:47:28 +0100

    fix

--- src/main/java/com/zap/scheduling/job/SendCodifiedLeadsEmailJob.java
+++ src/main/java/com/zap/scheduling/job/SendCodifiedLeadsEmailJob.java
@@ -34,7 +34,8 @@ public class SendCodifiedLeadsEmailJob implements Serializable {
 	private static final String JOB_NAME = "SEND_CODIFIED_LEADS_EMAIL";
 	private static final String USERNAME = "JOB";
 	private static final String EMAIL_SUBJECT_PREFIX = "AVANLEAD LEAD --- ";
-	private static final String TARGET_EMAIL = "dugt13@gmail.com";
+	private static final String TARGET_EMAIL = "leads@laboralgroup.com";
+//	private static final String TARGET_EMAIL = "dugt13@gmail.com";
 //	private static final String TARGET_EMAIL = "emanuel97gus@gmail.com";
 
 	private String TAG = "";
