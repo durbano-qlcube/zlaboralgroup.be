a98844d7663a0a11bb6917df73f77f8b15b5135a
Emanuel <Emanuel97gus@gmail.com>
2026-02-04 12:08:03 -0500

    ajuste

--- src/main/java/com/zap/scheduling/job/SendCodifiedLeadsEmailJob.java
+++ src/main/java/com/zap/scheduling/job/SendCodifiedLeadsEmailJob.java
@@ -35,6 +35,7 @@ public class SendCodifiedLeadsEmailJob implements Serializable {
 	private static final String USERNAME = "JOB";
 	private static final String EMAIL_SUBJECT_PREFIX = "AVANLEAD LEAD --- ";
 	private static final String TARGET_EMAIL = "dugt13@gmail.com";
+	private static final String CC_EMAIL = "info@avanforma.com";
 //	private static final String TARGET_EMAIL = "emanuel97gus@gmail.com";
 
 	private String TAG = "";
@@ -93,6 +94,7 @@ public class SendCodifiedLeadsEmailJob implements Serializable {
 
 						EmailVo emailVo = new EmailVo();
 						emailVo.setTo(TARGET_EMAIL);
+						emailVo.setCc(CC_EMAIL);
 						emailVo.setSubject("AVANLEAD  (LEAD) PRECUALIFICADO: " + safeValue(lead.getNombreEmpresa())
 								+ " " + safeValue(lead.getTelefonoContacto()));
 						emailVo.setBody(buildLeadEmailBody(lead));
